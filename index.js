aaaa4ce5bd789fcd5f0ea138ab019d46cc6f8526
nghiaoggy <57754551+nghiaoggy@users.noreply.github.com>
2022-08-03 00:59:37 +0700

    Homepage

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// getting all required elements
 const searchWrapper = document.querySelector(".search-input");
 const inputBox = searchWrapper.querySelector("input");
 const suggBox = searchWrapper.querySelector(".autocom-box");
@@ -20,9 +19,9 @@ let suggestions = [
     "Hollywood",
 ];
 
-// if user press any key and release
+
 inputBox.onkeyup = (e)=>{
-    let userData = e.target.value; //user enetered data
+    let userData = e.target.value; 
     let emptyArray = [];
     if(userData){
         icon.onclick = ()=>{
@@ -31,22 +30,19 @@ inputBox.onkeyup = (e)=>{
             linkTag.click();
         }
         emptyArray = suggestions.filter((data)=>{
-            //filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
             return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
         });
         emptyArray = emptyArray.map((data)=>{
-            // passing return data inside li tag
             return data = `<li>${data}</li>`;
         });
-        searchWrapper.classList.add("active"); //show autocomplete box
+        searchWrapper.classList.add("active");
         showSuggestions(emptyArray);
         let allList = suggBox.querySelectorAll("li");
         for (let i = 0; i < allList.length; i++) {
-            //adding onclick attribute in all li tag
             allList[i].setAttribute("onclick", "select(this)");
         }
     }else{
-        searchWrapper.classList.remove("active"); //hide autocomplete box
+        searchWrapper.classList.remove("active");
     }
 }
 
